de34f6d393d7f0dea637490a44f8146ef51173fd
NIU1748110 <oleguer.vilamala@autonoma.cat>
2025-04-10 19:39:08 +0200

    Update posicio.h

---
+++ posicio.h
@@ -0,0 +1,24 @@
+#ifndef POSICIO_H
+#define POSICIO_H
+#include <string>
+using namespace std;
+
+class Posicio
+{
+public:
+	Posicio() : m_fila(0), m_columna(0) {}
+	Posicio(int fila, int columna) : m_fila(fila), m_columna(columna) {}
+	Posicio(const string& posicio); 
+	int getFila() const { return m_fila; }
+	int getColumna() const { return m_columna; }
+	void setFila(int fila) { m_fila = fila; }
+	void setColumna(int columna) { m_columna = columna; }
+	string toString() const;
+	bool operator==(const Posicio& posicio) const;
+
+private:
+	int m_columna;
+	int m_fila;
+};
+
+#endif
