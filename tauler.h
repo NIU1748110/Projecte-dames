897a043a701bfe83561054dc01e2df61e0587e8e
NIU1746460 <joel.cereza@autonoma.cat>
2025-04-11 09:48:14 +0200

    Update tauler.h

--- tauler.h
+++ tauler.h
@@ -1,1 +1,29 @@
-#pragma once
+#ifndef TAULER_H
+#define TAULER_H
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "fitxa.h"
+#include "posicio.h"
+#include "moviment.h"
+
+const int N_FILES = 8;
+const int N_COLUMNES = 8;
+
+using namespace std;
+
+class Tauler
+{
+public:
+	void inicialitza(const string& nomFitxer);
+	void actualitzaMovimentsValids();
+	void getPosicionsPossibles(cont Posicio& origen,
+		int& nPosicions, Posicio posicionsPossibles[]);
+	bool mouFitxa(const Posicio& origen, const Posicio& desti);
+	string toString() const;
+
+private:
+	Fitxa m_tauler[N_FILES][N_COLUMNES];
+};
+
+#endif
