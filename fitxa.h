11f5f4352d842371439071ccd91b480fad6e43f0
NIU1748110 <oleguer.vilamala@autonoma.cat>
2025-04-11 09:42:43 +0200

    Update fitxa.h

--- fitxa.h
+++ fitxa.h
@@ -1,1 +1,36 @@
-#pragma once
+#ifndef FITXA_H
+#define FITXA_H
+#include "posicio.h"
+#include "moviment.h"
+
+const int N_FILES = 8;
+const int N_COLUMNES = 8;
+
+typedef enum
+{
+	TIPUS_NORMAL,
+	TIPUS_DAMA,
+	TIPUS_EMPTY
+} TipusFitxa;
+
+typedef enum
+{
+	COLOR_NEGRE,
+	COLOR_BLANC,
+} ColorFitxa;
+
+class Fitxa
+{
+public:
+	Fitxa(ColorFitxa color, TipusFitxa tipus) : m_color(color), m_tipus(tipus) {}
+
+	
+private:
+	ColorFitxa m_color;
+	TipusFitxa m_tipus;
+	Moviment m_movimentsValids[];
+
+};
+
+
+#endif
