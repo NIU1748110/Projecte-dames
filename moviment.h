6546a1f717c22815ae9aae1f449886ea382896df
NIU1746460 <joel.cereza@autonoma.cat>
2025-04-30 19:47:05 +0200

    Update moviment.h

--- moviment.h
+++ moviment.h
@@ -1,9 +1,21 @@
 #ifndef MOVIMENT_H
 #define MOVIMENT_H
+#include "posicio.h"
+
+const int MAX_PASSOS = 8;
+
 
 class Moviment
 {
+public:
+    Moviment() : m_numPassos(0) {}
+    void afegeixPas(const Posicio& p);
+    int getNumPassos() const { return m_numPassos; }
+    Posicio getPas(int i) const { return m_passos[i]; }
 
+private:
+    Posicio m_passos[MAX_PASSOS];
+    int m_numPassos;
 };
 
 #endif
