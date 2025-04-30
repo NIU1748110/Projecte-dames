577b4d47c467575549a99a48eb6cf0e8a06afa95
NIU1746460 <joel.cereza@autonoma.cat>
2025-04-30 19:46:39 +0200

    Update fitxa.cpp

--- fitxa.cpp
+++ fitxa.cpp
@@ -1,1 +1,14 @@
 #include "fitxa.h"
+
+void Fitxa::convertirEnDama()
+{
+    if (m_tipus == TIPUS_NORMAL)
+        m_tipus = TIPUS_DAMA;
+}
+
+
+void Fitxa::afegeixMoviment(const Moviment& moviment) 
+{
+    if (m_numMoviments < MAX_MOVIMENTS)
+        m_movimentsValids[m_numMoviments++] = moviment;
+}
