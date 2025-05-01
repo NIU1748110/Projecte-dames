42eee183f1ecd906b5b955a8ab249acf1d88287d
NIU1746460 <joel.cereza@autonoma.cat>
2025-05-01 22:22:23 +0200

    Update tauler.h

--- tauler.h
+++ tauler.h
@@ -3,9 +3,9 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include "fitxa.h"
-#include "posicio.h"
-#include "moviment.h"
+#include "fitxa.hpp"
+#include "posicio.hpp"
+#include "moviment.hpp"
 
 class Tauler
 {
@@ -14,8 +14,15 @@ public:
 	void actualitzaMovimentsValids();
 	void getPosicionsPossibles(const Posicio& origen,
 		int& nPosicions, Posicio posicionsPossibles[]);
-	bool mouFitxa(const Posicio& origen, const Posicio& desti) { return true; }
+	bool mouFitxa(const Posicio& origen, const Posicio& desti);
 	string toString() const;
+	Posicio posicioDesDeIndexos(int filaInterna, int columna) const;
+	void movimentsDames(Fitxa& fitxa, int fila, int columna);
+	void movimentsFitxesNormals(Fitxa& fitxa, int fila, int columna);
+	bool esMovimentValid(int fila, int columna) const;
+	bool esCasellaBuida(int fila, int columna) const;
+	bool esEnemic(const Fitxa& fitxa, int fila, int columna) const;
+	void afegirMoviment(Fitxa& fitxa, int filaDesti, int columnaDesti);
 
 private:
 	Fitxa m_tauler[N_FILES][N_COLUMNES];
