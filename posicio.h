a7174179aa40803d0d0315eca6a020e973eadb7e
NIU1746460 <joel.cereza@autonoma.cat>
2025-05-01 22:20:49 +0200

    Update posicio.h

--- posicio.h
+++ posicio.h
@@ -1,6 +1,7 @@
 #ifndef POSICIO_H
 #define POSICIO_H
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Posicio
@@ -8,7 +9,7 @@ class Posicio
 public:
 	Posicio() : m_fila(0), m_columna(0) {}
 	Posicio(int fila, int columna) : m_fila(fila), m_columna(columna) {}
-	Posicio(const string& posicio); 
+	Posicio(const string& posicio);
 	int getFila() const { return m_fila; }
 	int getColumna() const { return m_columna; }
 	void setFila(int fila) { m_fila = fila; }
