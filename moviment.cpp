bdb184211fed02a9fdb6628ab267b854b916d449
NIU1748110 <oleguer.vilamala@autonoma.cat>
2025-05-01 11:32:39 +0200

    Update moviment.cpp

--- moviment.cpp
+++ moviment.cpp
@@ -1,6 +1,7 @@
 #include "moviment.h"
 
-void Moviment::afegeixPas(const Posicio& p) {
+void Moviment::afegeixPas(const Posicio& p) 
+{
     if (m_numPassos < MAX_PASSOS) {
         m_passos[m_numPassos++] = p;
     }
