1a95b2174c5c5fb8fb25feabc9e30393d07217f4
NIU1746460 <joel.cereza@autonoma.cat>
2025-05-01 22:17:38 +0200

    Update fitxa.cpp

--- fitxa.cpp
+++ fitxa.cpp
@@ -1,4 +1,4 @@
-#include "fitxa.h"
+#include "fitxa.hpp"
 
 void Fitxa::convertirEnDama()
 {
@@ -11,4 +11,21 @@ void Fitxa::afegeixMoviment(const Moviment& moviment)
 {
     if (m_numMoviments < MAX_MOVIMENTS)
         m_movimentsValids[m_numMoviments++] = moviment;
+}
+
+bool Fitxa::teCaptura() const 
+{
+    for (int i = 0; i < m_numMoviments; ++i)
+    {
+        Moviment m = m_movimentsValids[i];
+        if (m.getNumPassos() > 0) 
+        {
+            Posicio origen = m.getPas(0);
+            Posicio desti = m.getPas(m.getNumPassos() - 1);
+            int df = abs(origen.getFila() - desti.getFila());
+            int dc = abs(origen.getColumna() - desti.getColumna());
+            if (df > 1 && dc > 1) return true;
+        }
+    }
+    return false;
 }
