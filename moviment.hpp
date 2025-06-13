12272910cd609410e0f1968dbb32548af8fd9f20
NIU1746460 <joel.cereza@autonoma.cat>
2025-06-13 21:21:17 +0200

    Update moviment.hpp

--- moviment.hpp
+++ moviment.hpp
@@ -1,21 +1,17 @@
 #ifndef MOVIMENT_H
 #define MOVIMENT_H
 #include "posicio.hpp"
-
-const int MAX_PASSOS = 8;
-
+#include <vector>
 
 class Moviment
 {
 public:
-    Moviment() : m_numPassos(0) {}
     void afegeixPas(const Posicio& p);
-    int getNumPassos() const { return m_numPassos; }
+    int getNumPassos() const { return m_passos.size(); }
     Posicio getPas(int i) const { return m_passos[i]; }
 
 private:
-    Posicio m_passos[MAX_PASSOS];
-    int m_numPassos;
+    vector<Posicio> m_passos;
 };
 
 #endif
